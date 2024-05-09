1c9974b8f18910917a919d6845257e64eabcf120
matheussASR <rm551466@fiap.com>
2024-05-09 12:50:41 -0300

    feat: adicionando components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import ProductsList from './components/ProductsList'
 
 function App() {
 
   return (
     <>
-      
+      <Header/>
+      <ProductsList/>
+      <Footer/>
     </>
   )
 }
