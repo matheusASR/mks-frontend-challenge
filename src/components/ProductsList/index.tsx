6b2541bc9bad4d29be673bb5aa8e253e1e0a877c
matheussASR <rm551466@fiap.com>
2024-05-09 12:42:05 -0300

    feat: instalando axios e styled-components

---
+++ src/components/ProductsList/index.tsx
@@ -0,0 +1,11 @@
+import { StyledProductsList } from './style';
+
+const ProductsList = () => {
+  return (
+    <StyledProductsList>
+      
+    </StyledProductsList>
+  );
+};
+
+export default ProductsList;
